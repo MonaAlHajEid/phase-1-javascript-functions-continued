c7677b8c04502f68886f523112fc1cabd2084c69
Mona <monabadee@gmail.com>
2023-11-19 19:29:53 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+
+// Define a Function Using Function Declaration
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Define Function Expression
+  const mondayWork = function (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Define Function Returning Function
+  const wrapAdjective = function (flair = '*') {
+    return function (adjective = 'special') {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  };
+  
+  // Export functions for testing
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective,
+  };
+  
